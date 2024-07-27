f2465f14f5cd5561efa651e355b063f845126134
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-07-28 01:18:21 +0530

    Some Basic Clap Commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::{command, Arg};
+
 fn main() {
-    
+
+    // Allows you to build the Command instance from your Cargo.toml at compile time.
+    let match_result = command!().arg(
+        Arg::new("firstname").short('f').long("firstname")
+    )
+    .arg(
+        Arg::new("lastname").short('l')
+    )
+    .arg(
+        Arg::new("fluffy").short('F')
+    )
+    .get_matches();
 }
