29b5458febc615e82f256aa2cdb29f936437664b
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-07-28 23:58:31 +0530

    Code Cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,5 @@ use commands::cd;
 
 fn main() {
 
-    // Allows you to build the Command instance from your Cargo.toml at compile time.
-    let match_result = command!()
-    .arg(
-        Arg::new("firstname").short('f').long("firstname")
-    )
-    .arg(
-        Arg::new("lastname").short('l')
-    )
-    .arg(
-        Arg::new("fluffy").short('F')
-    )
-    .get_matches();
-
     cd::execute_cd();
 }
